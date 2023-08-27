d4ff4d54495f5e0c14e4635d5fbc36cec209a1af
Dmitry Davlyatshin <dmitry.davlyatshin@gmail.com>
2023-08-27 15:40:02 +0300

    Add utility function 'create_db_if_not_exists'

--- test/utils/db.py
+++ test/utils/db.py
@@ -31,3 +31,30 @@ async def drop_and_recreate_db(connection: asyncpg.Connection, db_name: str, db_
         db_name,
         db_owner,
     )
+
+
+@exception_handler
+async def create_db_if_not_exists(
+    connection: asyncpg.Connection,
+    db_name: str,
+    db_owner: str,
+) -> None:
+    """Create database with given name if it doesn't exist."""
+    db_exists = await connection.fetch(
+        """SELECT datname from pg_database WHERE datname = %1""", db_name
+    )
+    if len(db_exists) != 0:
+        return
+
+    await connection.execute(
+        """
+        CREATE DATABASE %1
+          WITH OWNER = %2
+               ENCODING = 'UTF8'
+               LC_COLLATE = 'en_US.UTF-8'
+               LC_CTYPE = 'en_US.UTF-8'
+               template = template0
+        """,
+        db_name,
+        db_owner,
+    )
